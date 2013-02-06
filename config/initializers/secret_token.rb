ad1a73ac0b836f576694847849b9a021559d0b97
Alfredo <cantorrodista@gmail.com>
2013-02-06 18:22:54 +0100

    Initial commit: app vacia + spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primeriti::Application.config.secret_token = 'b49395e9282f9251019afd00a5bb6c5a4fc6db0083ca5c6c4b44c9cf15cecf9cc03af0997dd8d74d758b3659594c8fc6aa7074423d0cc6a0b0d655900b8b3f54'
